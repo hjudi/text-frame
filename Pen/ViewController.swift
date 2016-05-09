44294f2b128cfdd25b80ae6472274ebc38c8f945
Haz <h@linesandconcepts.co>
2016-05-09 10:47:04 -0400

    Initial Commit

---
+++ Pen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pen
+//
+//  Created by Hazim Judi on 2016-05-09.
+//  Copyright © 2016 LC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
